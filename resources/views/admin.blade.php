bb0ad5b4850001dc0fda1f2c8888601dfe14e165
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-12-10 11:54:36 -0500

    app.css not found error fix

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -5,9 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Admin</title>
-    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
-    <script src="{{ mix('/js/app.js') }}"></script>
-
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body class="wrapper layout-fixed">
   <x-nav-bar/>
@@ -17,5 +15,6 @@
   </div>
   <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+  <script src="/js/app.js"></script>
 </body>
 </html>
