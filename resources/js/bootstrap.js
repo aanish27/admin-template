79f3b98ee6f32e209893b683ea120fb8b8b6a2f7
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-12-11 11:25:19 -0500

    integrated admin-lte package

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,10 @@
 window._ = require('lodash');
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.$ = require("jquery");
 
-require("admin-lte");
 require("bootstrap");
 require("popper.js");
 require("../assets/fontawesome/js/all.min.js");
+require("../assets/adminlte/js/adminlte.min.js");
+require("../assets/adminlte/plugins/js/jquery.min.js");
+require("../assets/adminlte/plugins/js/popper.min.js");
