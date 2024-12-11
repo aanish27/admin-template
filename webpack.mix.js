3efea183c50e407a05bb2990c9c2eaee45c2ec7b
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-12-11 12:53:34 -0500

    webpack configured

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,10 @@
 const mix = require('laravel-mix');
 
-mix.js("resources/js/app.js", "public/js").css(
-    "resources/css/app.css",
-    "public/css"
-);
+mix.js("resources/js/app.js", "public/js")
+    .css("resources/css/app.css", "public/css")
+    .browserSync({
+        proxy: 'http://127.0.0.1:8000/',
+        open: false
+    });
 
-
+mix.disableNotifications();
