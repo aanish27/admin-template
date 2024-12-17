040ff7fc47398112dbd983f262d7d44fe133b037
Aanish Ramzan <110470875+aanish27@users.noreply.github.com>
2024-12-17 10:30:42 -0500

    index redirect to dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::middleware(['auth'])->group(function () {
-    Route::get('/', function () {
+    Route::redirect('/', 'dashboard');
+    Route::get('dashboard', function () {
         return view('dashboard.admin');
-    });
+    })->name('dashboard');
     Route::get('logout', [LoginController::class, 'logout'])->name('log-out');
 });
 
